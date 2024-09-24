1da3b57710f5aaae19520f6657bd8b02a1d17e23
Afira <afiratahmali@gmail.com>
2024-09-24 21:38:37 +0800

    added Pledge model and associated serializers, views and URLS

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -7,3 +8,14 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
